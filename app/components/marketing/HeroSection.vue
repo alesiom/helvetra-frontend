0d2432d189796eee639299de674d21fc03fc8bdc
Alex Margot <alesiom@gmail.com>
2025-12-14 07:12:18 +0100

    feat: increase translator component size for better screen utilization
    Closes helvetra/frontend#34

--- app/components/marketing/HeroSection.vue
+++ app/components/marketing/HeroSection.vue
@@ -4,7 +4,7 @@
 -->
 <template>
   <section class="pt-8 pb-16 md:pt-12 md:pb-24">
-    <div class="max-w-4xl mx-auto px-4 md:px-8">
+    <div class="max-w-6xl mx-auto px-4 md:px-6">
       <TranslationBox />
     </div>
   </section>
